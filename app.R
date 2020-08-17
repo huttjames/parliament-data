053e35a7a2656c1b60c4d60c70b440e2d2e4e54b
James Hutt <jhutt@g.harvard.edu>
2020-08-17 10:56:50 +0100

    Basic shiny app working

--- app.R
+++ app.R
@@ -5,33 +5,38 @@ library(shinythemes)
 library(vembedr)
 library(tidyverse)
 
+make_plot_master <- function(){ggplot(iris, aes(Sepal.Length, Sepal.Width)) + geom_point()}
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(fluidPage(theme = shinytheme("flatly"),
                           
-                          # Adding the favicon using code from Bridger Gordons project for the HTML
+                          # Adding title and favicon
                           
                           HTML('<script> document.title = "Parliament Speaks"; </script>'),
                           tags$head(tags$link(rel="shortcut icon", href="icon.ico")),
-                          navbarPage(
-                              "Parliament Speaks",
-                              tabPanel("4"),
-                              tabPanel("3"),
-                              tabPanel("2"),
-                              tabPanel("1")
-                          ))
-)
+  navbarPage(
+      "Parliament Speaks",
+      tabPanel("4",
+               sidebarLayout(
+                   sidebarPanel(
+                    htmlOutput("about_me"), 
+                    img(src="headshot.jpg",width="100%"), 
+                    width = 3),
+                   mainPanel(
+                       plotOutput("netwPlot")
+                   ))),
+      tabPanel("3"),
+      tabPanel("2"),
+      tabPanel("1")
+  )
+))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    output$netwPlot <- renderPlot({make_plot_master()}, width = 900, height = 900, execOnResize = TRUE)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    output$about_me <- renderUI({HTML("<b> About the Developer </b> <br/><br/>")})
 }
 
 # Run the application 
