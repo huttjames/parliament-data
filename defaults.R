b22d03f17a8ad94d5179f70094d9f0341fc3677b
James Hutt <jhutt@g.harvard.edu>
2020-08-20 22:41:13 +0100

    defaults stored in cleaning and defaults.r

---
+++ defaults.R
@@ -0,0 +1,15 @@
+# Assign each party a colour 
+
+party_colours <<- c("Con" = "#0a3b7d",
+          "Lab" = "#e4003b",
+          "LibDem" = "#fbb347",
+          "SNP" = "#fcf38e",
+          "DUP" = "#24303c",
+          "PlaidCymru" = "#006a56",
+          "UUP" = "#a0cef2",
+          "SDLP" = "#00704f",
+          "Change UK" = "black",
+          "UKIP" = "#702d85",
+          "GPEW" = "#6bb13c",
+          "Independent" = "black",
+          "Other" = "grey")
